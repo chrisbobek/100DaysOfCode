c2766d8a73117ec6aa4f7d275402cd72bed11020
chrisbobek <chrisbobek@gmail.com>
2020-11-08 18:48:37 -0800

    Adds more questions

--- treasure-island.py
+++ treasure-island.py
@@ -12,7 +12,7 @@ wmd = wmd.lower()
 if wmd == "right":
 	print("Game Over. Get back to us when those WMDs turn up.\n")
 else:
-	guns = input("Were you convinced that Barack Obama was on the verge of confiscating guns and imprisoning gun owners in concentration camps? [fire or safety]\n")
+	guns = input("Were you convinced that Barack Obama was on the verge of confiscating guns and imprisoning gun owners in concentration camps? [fire or safety on]\n")
 	if guns.lower() == "fire":
 		print("Game Over. Alex Jones is entertaining but always DYOR.\n")
 	else:
@@ -21,7 +21,11 @@ else:
 		if infowars.lower() == "yes":
 			print("Game Over. Wow! You're a hardcore InfoWars fan.\n")
 		else:
-			print("else")	
-
-source = input("Which of these news sources do you trust the most? [red for InfoWars; ue for CNN; yellow for Al Jazeera]")
+			source = input("Which of these news sources do you trust the most? [red for InfoWars; ue for CNN; yellow for 'I always maintain healty scepticism.']\n")
+			if source.lower() == "red":
+				print("Game Over. Always DYOR!\n")
+			elif source.lower() == "blue":
+				print("Game Over. CNN tries hard but always DYOR.\n")
+			elif source.lower() == "yellow":
+				print("You're awesome for not shunning sources that disagree with your world view! Keep up the good work!\n")
 
