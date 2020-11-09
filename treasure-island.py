8de29b7a09e30e731e6649189cb137104c98dde9
chrisbobek <chrisbobek@gmail.com>
2020-11-08 17:58:31 -0800

    Adds questions

--- treasure-island.py
+++ treasure-island.py
@@ -1,4 +1,18 @@
 # #100DaysOfCode #AppBrewery #Python
 
+print('''
+# insert ASCII art here
+''')
+
 print("Welcome to Misinformation Island!")
 print("Your mission is to find the truth--or to get as close to it as you can.") 
+
+wmd = input("Were you convinced that Iraq had WMDs in 2003? [right or left]")
+
+guns = input("Were you convinced that Barack Obama was on the verge of confiscating guns and imprisoning gun owners in concentration camps? [fire or safety]")
+
+infowars = input("Are you convinced that Sandy Hook was a false flag operation? [yes or no]")
+qanon = input("Are you convinced that Hillary Rodham Clinton is running a child sex trafficking ring out of D.C.'s Comet pizza parlor? [yes or no]")
+
+source = input("Which of these news sources do you trust the most? [red for InfoWars; ue for CNN; yellow for Al Jazeera]")
+
