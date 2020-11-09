ee01ea60cce6c63b38b468d8c21db273d3138fcb
chrisbobek <chrisbobek@gmail.com>
2020-11-08 18:33:36 -0800

    Adds questions

--- treasure-island.py
+++ treasure-island.py
@@ -1,18 +1,27 @@
-# #100DaysOfCode #AppBrewery #Python
+#100DaysOfCode #AppBrewery #Python
 
 print('''
 # insert ASCII art here
 ''')
 
 print("Welcome to Misinformation Island!")
-print("Your mission is to find the truth--or to get as close to it as you can.") 
+print("Your mission is to find the truth--or to get as close to it as you can.\n") 
 
-wmd = input("Were you convinced that Iraq had WMDs in 2003? [right or left]")
+wmd = input("Were you convinced that Iraq had WMDs in 2003? [right]\n")
+wmd = wmd.lower()
+if wmd == "right":
+	print("Game Over. Get back to us when those WMDs turn up.\n")
+else:
+	guns = input("Were you convinced that Barack Obama was on the verge of confiscating guns and imprisoning gun owners in concentration camps? [fire or safety]\n")
+	if guns.lower() == "fire":
+		print("Game Over. Alex Jones is entertaining but always DYOR.\n")
+	else:
+		infowars = input("Are you convinced that Sandy Hook was a false flag operation? [yes or no]\n")
 
-guns = input("Were you convinced that Barack Obama was on the verge of confiscating guns and imprisoning gun owners in concentration camps? [fire or safety]")
-
-infowars = input("Are you convinced that Sandy Hook was a false flag operation? [yes or no]")
-qanon = input("Are you convinced that Hillary Rodham Clinton is running a child sex trafficking ring out of D.C.'s Comet pizza parlor? [yes or no]")
+		if infowars.lower() == "yes":
+			print("Game Over. Wow! You're a hardcore InfoWars fan.\n")
+		else:
+			print("else")	
 
 source = input("Which of these news sources do you trust the most? [red for InfoWars; ue for CNN; yellow for Al Jazeera]")
 
