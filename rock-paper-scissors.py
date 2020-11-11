768ccb5ac8f6a2e96e3b8b9291a46c6c4a95f984
chrisbobek <chrisbobek@gmail.com>
2020-11-11 06:08:18 -0800

    Removes print() functions used to test input

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -40,16 +40,15 @@ scissors = '''
 
 options = [rock, paper, scissors]
 
-rock_row = ['Tie', 'Paper beats rock! Player wins!', 'Rock beats scissors! Player loses.']
-paper_row = ['Paper beats rock! Player loses.', 'Tie', 'Scissors beats paper! Player wins!'] 
-scissors_row = ['Rock beats scissors! Player wins!', 'Scissors beat paper! Player loses.', 'Tie'] 
+rock_row = ['Tie!\n', 'Paper beats rock! Player loses.\n', 'Rock beats scissors! Player loses.\n']
+paper_row = ['Paper beats rock! Player wins!\n', 'Tie!\n', 'Scissors beats paper! Player loses.\n'] 
+scissors_row = ['Rock beats scissors! Player loses.\n', 'Scissors beat paper! Player wins!\n', 'Tie!\n'] 
    
 grid = [rock_row, paper_row, scissors_row]
 
-instructions = "\nType 0 to throw down a rock, 1 to throw down paper, or 2 to throw down scissors. Go!\n"
+instructions = "\nType 0 to throw down a rock, 1 to throw down paper, or 2 to throw down scissors.\nGo!\n"
 player = input(instructions)
 player = int(player)
-
 print("\nYou chose:")
 print(options[player])
 
@@ -57,5 +56,5 @@ system = random.randint(0,2)
 print("\nThe system chose:")
 print(options[system])
 
-grid[player][system]
+print(grid[player][system])
 
