057af27cb5d30641fa40a0d84bf3bccca5f26f3b
chrisbobek <chrisbobek@gmail.com>
2020-11-13 06:09:28 -0800

    Fixes incomplete condition

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,12 +1,13 @@
 # FizzBuzz
 
 for number in range(1, 101):
+
 	if number % 3 == 0:
 		if number % 5 == 0:
 			print("FizzBuzz")
 		else:
 			print("Fizz")
-	elif number % 5:
+	elif number % 5 == 0:
 		print("Buzz")
 	else:
 		print(number)	
